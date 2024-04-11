0168307ddb105ec4bf36ad51c5039ac03a7051b8
Børge Bjørnstadjordet <borgetull@gmail.com>
2024-04-11 17:40:18 +0200

    ADded handout

---
+++ eksam/handout/d1_udp.c
@@ -0,0 +1,78 @@
+/* ======================================================================
+ * YOU ARE EXPECTED TO MODIFY THIS FILE.
+ * ====================================================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include "d1_udp.h"
+
+
+D1Peer* d1_create_client( )
+{
+    // creates the UDP socket
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) return NULL;
+
+    // allocates space on heap for D1Peer, which is assigned socket and returned
+    D1Peer* client = malloc(sizeof(D1Peer));
+    if (client != NULL) {
+        client->socket = sockfd;
+        return client;
+    }
+    return NULL;
+}
+
+D1Peer* d1_delete( D1Peer* peer )
+{
+    // if non-NULL, close socket and free memory
+    if (peer != NULL) {
+        if (peer->socket != -1) {
+            close(peer->socket);
+        }
+        free(peer);
+    }
+    return NULL;
+}
+
+int d1_get_peer_info( struct D1Peer* peer, const char* peername, uint16_t server_port )
+{
+    /* implement this */
+    return 0;
+}
+
+int d1_recv_data( struct D1Peer* peer, char* buffer, size_t sz )
+{
+    /* implement this */
+    return 0;
+}
+
+int d1_wait_ack( D1Peer* peer, char* buffer, size_t sz )
+{
+    /* This is meant as a helper function for d1_send_data.
+     * When D1 data has send a packet, this one should wait for the suitable ACK.
+     * If the arriving ACK is wrong, it resends the packet and waits again.
+     *
+     * Implementation is optional.
+     */
+    return 0;
+}
+
+int d1_send_data( D1Peer* peer, char* buffer, size_t sz )
+{
+    /* implement this */
+    return 0;
+}
+
+void d1_send_ack( struct D1Peer* peer, int seqno )
+{
+    /* implement this */
+}
+
